6ee5f4def49e70ddb4a50c7b88baf303127ffd1f
Artur <shimkus.artur@mail.ru>
2025-01-22 22:30:20 +0300

    Маршруты бога

---
+++ python_hw/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('/', main, name='main'),
+    path('posts/', catalog_posts, name='catalog_posts'),
+    path('categories/', catalog_categories, name='catalog_categories'),
+    path('categories/<slug:category_slug>/', category_detail, name='category_detail'),
+    path('tags/', catalog_tags, name='catalog_tags'),
+    path('tags/<slug:tag_slug>/', tag_detail, name='tag_detail'),
+]
